c5e205399d32aba59c074928e718c6e596c3a40d
Susandinelli <susandinelli@gmail.com>
2023-01-19 17:24:37 -0500

    just server

---
+++ routes/sunday.js
@@ -0,0 +1,512 @@
+const express = require("express");
+
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const recordRoutes = express.Router();
+
+// This will help us connect to the database
+const dbo = require("../db/conn");
+
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
+// This section will help you get a list of all the records.
+recordRoutes.route("/sunday").get(function (req, res) {
+  let db_connect = dbo.getDb("users");
+  db_connect
+    .collection("sunday")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single record by id
+recordRoutes.route("/sunday/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("sunday").findOne(myquery, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+// This section will help you create a new record.
+recordRoutes.route("/sunday/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    Task: req.body.Task,
+  };
+  db_connect.collection("sunday").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// This section will help you update a record by id.
+recordRoutes.route("/update/:id").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  let newvalues = {
+    $set: {
+      Task: req.body.Task,
+    },
+  };
+  db_connect
+    .collection("sunday")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+recordRoutes.route("/sunday/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("sunday").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
+
+//MONDAY
+
+recordRoutes.route("/monday").get(function (req, res) {
+  let db_connect = dbo.getDb("users");
+  db_connect
+    .collection("monday")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single record by id
+recordRoutes.route("/monday/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("monday").findOne(myquery, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+// This section will help you create a new record.
+recordRoutes.route("/monday/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    Task: req.body.Task,
+  };
+  db_connect.collection("monday").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// This section will help you update a record by id.
+recordRoutes.route("/monday/:id").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  let newvalues = {
+    $set: {
+      Task: req.body.Task,
+    },
+  };
+  db_connect
+    .collection("monday")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+recordRoutes.route("/monday/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("monday").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
+
+recordRoutes.route("/monday").get(function (req, res) {
+  let db_connect = dbo.getDb("users");
+  db_connect
+    .collection("monday")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single record by id
+recordRoutes.route("/monday/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("monday").findOne(myquery, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+// This section will help you create a new record.
+recordRoutes.route("/monday/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    Task: req.body.Task,
+  };
+  db_connect.collection("monday").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// This section will help you update a record by id.
+recordRoutes.route("/monday/:id").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  let newvalues = {
+    $set: {
+      Task: req.body.Task,
+    },
+  };
+  db_connect
+    .collection("monday")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+recordRoutes.route("/monday/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("monday").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
+
+//TUESDAY
+
+recordRoutes.route("/tuesday").get(function (req, res) {
+  let db_connect = dbo.getDb("users");
+  db_connect
+    .collection("tuesday")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single record by id
+recordRoutes.route("/tuesday/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("tuesday").findOne(myquery, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+// This section will help you create a new record.
+recordRoutes.route("/tuesday/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    Task: req.body.Task,
+  };
+  db_connect.collection("tuesday").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// This section will help you update a record by id.
+recordRoutes.route("/tuesday/:id").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  let newvalues = {
+    $set: {
+      Task: req.body.Task,
+    },
+  };
+  db_connect
+    .collection("tuesday")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+recordRoutes.route("/tuesday/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("tuesday").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
+
+//WEDNESDAY
+recordRoutes.route("/wednesday").get(function (req, res) {
+  let db_connect = dbo.getDb("users");
+  db_connect
+    .collection("wednesday")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single record by id
+recordRoutes.route("/wednesday/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("wednesday").findOne(myquery, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+// This section will help you create a new record.
+recordRoutes.route("/wednesday/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    Task: req.body.Task,
+  };
+  db_connect.collection("wednesday").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// This section will help you update a record by id.
+recordRoutes.route("/wednesday/:id").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  let newvalues = {
+    $set: {
+      Task: req.body.Task,
+    },
+  };
+  db_connect
+    .collection("wednesday")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+recordRoutes.route("/wednesday/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("wednesday").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
+//THURSDAY
+recordRoutes.route("/thursday").get(function (req, res) {
+  let db_connect = dbo.getDb("users");
+  db_connect
+    .collection("thursday")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single record by id
+recordRoutes.route("/thursday/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("thursday").findOne(myquery, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+// This section will help you create a new record.
+recordRoutes.route("/thursday/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    Task: req.body.Task,
+  };
+  db_connect.collection("thursday").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// This section will help you update a record by id.
+recordRoutes.route("/thursday/:id").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  let newvalues = {
+    $set: {
+      Task: req.body.Task,
+    },
+  };
+  db_connect
+    .collection("thursday")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+recordRoutes.route("/thursday/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("thursday").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
+
+//FRIDAY
+
+recordRoutes.route("/friday").get(function (req, res) {
+  let db_connect = dbo.getDb("users");
+  db_connect
+    .collection("friday")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single record by id
+recordRoutes.route("/friday/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("friday").findOne(myquery, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+// This section will help you create a new record.
+recordRoutes.route("/friday/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    Task: req.body.Task,
+  };
+  db_connect.collection("friday").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// This section will help you update a record by id.
+recordRoutes.route("/friday/:id").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  let newvalues = {
+    $set: {
+      Task: req.body.Task,
+    },
+  };
+  db_connect
+    .collection("friday")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+recordRoutes.route("/friday/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("friday").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
+
+//SATURDAY
+
+recordRoutes.route("/saturday").get(function (req, res) {
+  let db_connect = dbo.getDb("users");
+  db_connect
+    .collection("saturday")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+// This section will help you get a single record by id
+recordRoutes.route("/saturday/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("friday").findOne(myquery, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+// This section will help you create a new record.
+recordRoutes.route("/saturday/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    Task: req.body.Task,
+  };
+  db_connect.collection("saturday").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
+
+// This section will help you update a record by id.
+recordRoutes.route("/saturday/:id").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  let newvalues = {
+    $set: {
+      Task: req.body.Task,
+    },
+  };
+  db_connect
+    .collection("saturday")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+recordRoutes.route("/saturday/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId(req.params.id) };
+  db_connect.collection("saturday").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
+
+module.exports = recordRoutes;
